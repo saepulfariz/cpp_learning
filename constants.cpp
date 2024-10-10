b8ae5c420946f76df179baf6b53bc98f5015af44
saepulfariz <saepulfariz108@gmail.com>
2024-10-10 10:07:49 +0700

    add example constants define

---
+++ constants.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+#define NAME "Saepulfariz"
+#define PANJANG 8
+#define LEBAR 5
+
+int main()
+{
+    cout << "Nama = " << NAME;
+    cout << "\n";
+
+    cout << "Panjang = " << PANJANG;
+    cout << "\n";
+
+    cout << "Lebar = " << LEBAR;
+    cout << "\n";
+
+    cout << "Luas (panjang*lebar) = " << PANJANG * LEBAR;
+    cout << "\n";
+
+    return 0;
+}
