b10108b2bcbab005229a63b2d4d6d93b4d5664b0
saepulfariz <saepulfariz108@gmail.com>
2024-10-11 10:58:08 +0700

    add example function include file

---
+++ function_data.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHello(string name = "Saepulfariz", int age = 24)
+{
+    cout << "Hello World By " << name << "! My age " << age << "." << endl;
+}
---
+++ function_include.cpp
@@ -0,0 +1,11 @@
+#include "function_data.h"
+
+int main()
+{
+    sayHello();
+    sayHello("Zail");
+    sayHello("Zail");
+    sayHello("Zail");
+
+    return 0;
+}
