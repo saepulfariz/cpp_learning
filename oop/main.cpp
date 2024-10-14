22a6b5dd21118c82ac1297c878c952394ffe65eb
saepulfariz <saepulfariz108@gmail.com>
2024-10-14 11:26:50 +0700

    add create class

---
+++ oop/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+class Laptop {
+  // isi dari class Laptop...
+  // isi dari class Laptop...
+};
+ 
+int main()
+{
+  // kode program
+  // kode program
+  return 0;
+}
